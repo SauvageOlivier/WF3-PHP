7e52c22926071d2d07e9771bb5dd246b9e9cf89f
SauvageOlivier <oliver0062@yahoo.fr>
2018-02-21 14:36:08 +0100

    first
    1st test

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+$prenom = "Master";
+
+echo "<h1>";
+echo "bonjour je m'appelle $prenom\n";
+echo "<br>\n";
+echo "bonjour je m'appelle $prenom" . "\n";
+echo "<br>\n";
+echo "Bonjour je m'appelle" .$prenom . "\n";
+echo "</h1>\n";
+?>
+
